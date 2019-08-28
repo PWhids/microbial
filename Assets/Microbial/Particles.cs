0beb88d5ce0d648b7effef4c816adde2486a9971
Peter Whidden <all.cows.like.to.moo@gmail.com>
2019-08-27 21:57:18 -0700

    balls

---
+++ Assets/Microbial/Particles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Renderer r;
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
